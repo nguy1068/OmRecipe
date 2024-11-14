35ead34244bd147dbc2ab7c1adceb7c7ecaed75e
nguy1068 <nguy1068@algonquinlive.com>
2024-11-14 00:57:33 -0500

    UPDATE: DrinkListView change API

--- OmRecipe/Views/DrinkListView.swift
+++ OmRecipe/Views/DrinkListView.swift
@@ -2,14 +2,41 @@ import SwiftUI
 
 struct Drink: Identifiable, Codable {
     let id = UUID()
-    let name: String
-    let ingredients: [String]
-    let instructions: String
+    let idDrink: String
+    let strDrink: String
+    let strDrinkThumb: String
 
     enum CodingKeys: String, CodingKey {
-        case name
-        case ingredients
-        case instructions
+        case idDrink
+        case strDrink
+        case strDrinkThumb
+    }
+}
+
+struct DrinkDetail: Identifiable, Codable {
+    let id = UUID()
+    let idDrink: String
+    let strDrink: String
+    let strCategory: String?
+    let strAlcoholic: String?
+    let strGlass: String?
+    let strInstructions: String?
+    let strDrinkThumb: String?
+    let strIngredient1: String?
+    let strIngredient2: String?
+    let strIngredient3: String?
+
+    enum CodingKeys: String, CodingKey {
+        case idDrink
+        case strDrink
+        case strCategory
+        case strAlcoholic
+        case strGlass
+        case strInstructions
+        case strDrinkThumb
+        case strIngredient1
+        case strIngredient2
+        case strIngredient3
     }
 }
 
@@ -17,6 +44,7 @@ struct DrinkListView: View {
     @State private var drinks: [Drink] = []
     @State private var isLoading = true
     @State private var errorMessage: String?
+    @State private var selectedDrinkDetail: DrinkDetail?
 
     var body: some View {
         NavigationStack {
@@ -30,12 +58,21 @@ struct DrinkListView: View {
                 } else {
                     List(drinks) { drink in
                         VStack(alignment: .leading) {
-                            Text(drink.name)
+                            Text(drink.strDrink)
                                 .font(.headline)
-                            Text("Ingredients: \(drink.ingredients.joined(separator: ", "))")
-                                .font(.subheadline)
-                            Text("Instructions: \(drink.instructions)")
-                                .font(.body)
+                            AsyncImage(url: URL(string: drink.strDrinkThumb)) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                            .cornerRadius(8)
+                            .clipped()
+                            Button("View Details") {
+                                fetchDrinkDetail(idDrink: drink.idDrink) { detail in
+                                    self.selectedDrinkDetail = detail
+                                }
+                            }
                         }
                         .padding(.vertical, 5)
                     }
@@ -45,23 +82,27 @@ struct DrinkListView: View {
             .onAppear {
                 fetchDrinks()
             }
+            .sheet(item: $selectedDrinkDetail) { detail in
+                DrinkDetailView(drinkDetail: detail)
+            }
         }
     }
 
     private func fetchDrinks() {
-        guard let url = URL(string: "https://api.api-ninjas.com/v1/cocktail?name=margarita") else {
+        guard
+            let url = URL(
+                string: "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+        else {
             return
         }
-        var request = URLRequest(url: url)
-        request.setValue(
-            "KlOwHnrc9G11N+KdMp95ZA==ehrOffBfa8hWwTol", forHTTPHeaderField: "X-Api-Key")
 
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
                 do {
-                    let decodedResponse = try JSONDecoder().decode([Drink].self, from: data)
+                    let decodedResponse = try JSONDecoder().decode(
+                        [String: [Drink]].self, from: data)
                     DispatchQueue.main.async {
-                        self.drinks = decodedResponse
+                        self.drinks = decodedResponse["drinks"]?.prefix(10).map { $0 } ?? []
                         self.isLoading = false
                     }
                 } catch {
@@ -77,5 +118,93 @@ struct DrinkListView: View {
                 }
             }
         }.resume()
+    }
+
+    private func fetchDrinkDetail(idDrink: String, completion: @escaping (DrinkDetail?) -> Void) {
+        guard
+            let url = URL(
+                string: "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=\(idDrink)")
+        else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode(
+                        [String: [DrinkDetail]].self, from: data)
+                    DispatchQueue.main.async {
+                        completion(decodedResponse["drinks"]?.first)
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }.resume()
+    }
+}
+
+struct DrinkDetailView: View {
+    let drinkDetail: DrinkDetail
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(drinkDetail.strDrink)
+                .font(.largeTitle)
+                .padding(.bottom)
+            if let imageUrl = drinkDetail.strDrinkThumb, let url = URL(string: imageUrl) {
+                AsyncImage(url: url) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 200, height: 200)
+                .cornerRadius(8)
+                .clipped()
+                .padding(.bottom)
+            }
+            if let category = drinkDetail.strCategory {
+                Text("Category: \(category)")
+                    .font(.headline)
+                    .padding(.bottom)
+            }
+            if let alcoholic = drinkDetail.strAlcoholic {
+                Text("Alcoholic: \(alcoholic)")
+                    .font(.headline)
+                    .padding(.bottom)
+            }
+            if let glass = drinkDetail.strGlass {
+                Text("Glass: \(glass)")
+                    .font(.headline)
+                    .padding(.bottom)
+            }
+            if let instructions = drinkDetail.strInstructions {
+                Text("Instructions: \(instructions)")
+                    .font(.body)
+                    .padding(.bottom)
+            }
+            if let ingredient1 = drinkDetail.strIngredient1 {
+                Text("Ingredient 1: \(ingredient1)")
+                    .font(.body)
+                    .padding(.bottom)
+            }
+            if let ingredient2 = drinkDetail.strIngredient2 {
+                Text("Ingredient 2: \(ingredient2)")
+                    .font(.body)
+                    .padding(.bottom)
+            }
+            if let ingredient3 = drinkDetail.strIngredient3 {
+                Text("Ingredient 3: \(ingredient3)")
+                    .font(.body)
+                    .padding(.bottom)
+            }
+        }
+        .padding()
     }
 }
