055774fc2ae4f1b07437db30e174a77d6e663966
nguy1068 <nguy1068@algonquinlive.com>
2024-11-13 21:11:34 -0500

    ADD: Splash screen

--- OmRecipe/OmRecipeApp.swift
+++ OmRecipe/OmRecipeApp.swift
@@ -12,7 +12,7 @@ struct OmRecipeApp: App {
     @StateObject private var recipeData: RecipeData = .init()
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            SplashScreenView()
                 .environmentObject(recipeData)
         }
     }
---
+++ OmRecipe/Views/SplashScreenView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @EnvironmentObject var recipeData: RecipeData
+
+    var body: some View {
+        if isActive {
+            RecipeListView()
+                .environmentObject(recipeData)
+        } else {
+            VStack {
+                Image("omrecipe_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
